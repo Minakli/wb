af81a69dae020d0d8fce0df1844f35bdd3853886
Plan-D <t.9664610@gmail.com>
2023-08-08 13:06:42 +0500

    pa First pull

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 //Data
 import { arrTitle } from "../Data/Data.js";
@@ -12,7 +12,22 @@ import Main from "../components/Main/Main";
 
 export default function App() {
   const [btnIsActive, setBtnIsActive] = useState("Home");
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    fetch(
+      "https://statistics-api.wildberries.ru/api/v1/supplier/stocks?dateFrom=2023-04-12",
+      {
+        headers: {
+          Authorization:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NJRCI6ImNkZDNjNDg1LTkxYmItNDNiNi1iYzExLTQ1MzAzM2E3MDliOCJ9.TwQESCgtVYLIClyTh0A3zRI715V1YRN4hc9wb69H0Vw",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((actualData) => setData(actualData));
+  }, []);
+  console.log(data);
   return (
     <>
       <Header
